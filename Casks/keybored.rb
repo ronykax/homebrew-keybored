442b8041582fb6d223be8c83b481cd27adc784d1
Rony Kati <ronykax@gmail.com>
2026-05-21 18:59:47 +0530

    yes

---
+++ Casks/keybored.rb
@@ -0,0 +1,11 @@
+cask "keybored" do
+  version "1.0.0"
+  sha256 "d3b32d6afd1e9c2149ee20116f5c040ec00dabfc4d9710e6b5d582658213e06f"
+
+  url "https://github.com/ronykax/keybored/releases/download/v#{version}/Keybored.zip"
+  name "Keybored"
+  desc "A killer utility built in Swift"
+  homepage "https://github.com/ronykax/keybored"
+
+  app "Keybored.app"
+end

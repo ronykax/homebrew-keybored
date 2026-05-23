5f855ff667b54440e25d6621b787ab4e7b9b351f
Rony Kati <73883742+ronykax@users.noreply.github.com>
2026-05-23 17:14:17 +0530

    Update SHA256 checksum for keybored cask

--- Casks/keybored.rb
+++ Casks/keybored.rb
@@ -1,6 +1,6 @@
 cask "keybored" do
   version "1.0.2"
-  sha256 "79e664d54fc01677779c7b34561e0f64ad4a6f52df80064017fb509d5b5665c7"
+  sha256 "68c35fe040c9b3322e6cf8d3719c3e1a0997f6bab5fac19afa34ab178f5b85e0"
 
   url "https://github.com/ronykax/keybored/releases/download/v#{version}/Keybored.zip"
   name "Keybored"
